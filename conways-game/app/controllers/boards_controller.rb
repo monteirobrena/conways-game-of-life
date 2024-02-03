b1a2699c290f0d81fcfcc04d08ca23702c501aab
Brena Monteiro <monteirobrena@gmail.com>
2024-02-03 17:39:55 +0000

    Implemented create to board.

--- conways-game/app/controllers/boards_controller.rb
+++ conways-game/app/controllers/boards_controller.rb
@@ -1,7 +1,16 @@
 class BoardsController < ApplicationController
+  def board_params
+    params.require(:board).permit([:size, :attempts])
+  end
 
-    # POST /boards
-    def create
-        render json: { }, status: :created
+  # POST /boards
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      render json: { id: @board.id }, status: :created
+    else
+      render json: @board.errors, status: :unprocessable_entity
     end
+  end
 end
--- conways-game/spec/requests/boards_spec.rb
+++ conways-game/spec/requests/boards_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Boards", type: :request do
   describe "POST /create" do
-    it "returns http created" do
-      headers = { "ACCEPT" => "application/json" }
+    headers = { "ACCEPT" => "application/json" }
+
+    it "returns success when create" do
       post "/boards#create", params: { board: { size: 30, attempts: 10 } }, headers: headers
 
       expect(response.content_type).to eq("application/json; charset=utf-8")
       expect(response).to have_http_status(:created)
+      expect(response.body).to eq({ id: 1 }.to_json)
+    end
+
+    it "returns error when unable to create" do
+      post "/boards#create", params: { board: { attempts: 10 } }, headers: headers
+
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.body).to eq({ size: ["can't be blank"] }.to_json)
     end
   end
 end
