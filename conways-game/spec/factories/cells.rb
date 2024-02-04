66014d9329ed6aaae50d8b1e54473325c5d8b786
Brena Monteiro <monteirobrena@gmail.com>
2024-02-04 12:05:15 +0000

    Added FactoryBot. Remove test folder.

---
+++ conways-game/spec/factories/cells.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :cell do
+    alive { false }
+    x_position { 1 }
+    y_position { 1 }
+  end
+end
--- conways-game/spec/models/board_spec.rb
+++ conways-game/spec/models/board_spec.rb
@@ -5,4 +5,18 @@ describe Board do
 
   it { should validate_presence_of :size }
   it { should validate_presence_of :attempts }
+
+  describe "creation with cells" do
+    cells = 
+
+    it "should build the board" do
+      board = Board.new(
+        size: 30,
+        attempts: 10,
+        cells: cells
+      )
+
+      expect(board).to be_valid
+    end
+  end
 end
